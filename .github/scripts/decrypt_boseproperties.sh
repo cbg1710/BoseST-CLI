52ceeed58a34f69e67a1771d4d8a6c2c5e826e0b
Christian Glaesel <cglaesel@hotmail.de>
2020-10-01 23:18:45 +0200

    Modified decyrption of properties

--- .github/scripts/decrypt_boseproperties.sh
+++ .github/scripts/decrypt_boseproperties.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$BOSE_PROPERTIES_KEY" \
---output $HOME/secrets/bose.properties bose.properties.gpg
+--output bose.properties bose.properties.gpg
